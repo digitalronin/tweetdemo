b18b430af190fd57a5a6ae71fc24179363e46345
digitalronin <david@digitalronin.com>
2022-04-07 15:35:44 +0900

    Enable creating new tweets
    Also, add validation to Tweet model

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,24 @@
 class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all
+    @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+
+    respond_to do |format|
+      if @tweet.save
+        format.html { redirect_to tweets_url, notice: 'Tweet was successfully created.' }
+      else
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:body)
   end
 end
